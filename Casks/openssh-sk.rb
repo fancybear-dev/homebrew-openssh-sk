d2413ad4f8f14cec0eadc5440387f509c1a04e87
fancybear-dev <78414914+fancybear-dev@users.noreply.github.com>
2025-06-23 18:27:00 +0000

    Update Homebrew Cask to version OpenSSH_10.0

--- Casks/openssh-sk.rb
+++ Casks/openssh-sk.rb
@@ -17,11 +17,16 @@ cask "openssh-sk" do
   caveats <<~EOS
     To enable the sk-libfido2-fancybear-dev library for SSH, you need to set the SSH_SK_PROVIDER environment variable.
 
-    Would you like to append 'export SSH_SK_PROVIDER=/usr/local/lib/sk-libfido2-fancybear-dev.dylib' to ~/.zshenv?
-
-    If yes, run the following command:
+    After installing this Cask, you can add the following line to your shell configuration file (e.g., ~/.zshenv or ~/.bash_profile):
       echo 'export SSH_SK_PROVIDER=/usr/local/lib/sk-libfido2-fancybear-dev.dylib' >> ~/.zshenv
+    Before testing it out, either source the file or restart your terminal:
       source ~/.zshenv
+
+    When running for the first time, Apple will refuse to execute it due to security mechanisms. 
+    
+    To allow the execution, select "Allow anyway" at "Settings" -> "Privacy & Security" -> "Security" (section below). 
+    
+    If the option is not available, ensure to first set "Allow applications from" to "App Store & Known Developers" at the same section.
   EOS
 
   uninstall delete: "/usr/local/lib/sk-libfido2-fancybear-dev.dylib"
